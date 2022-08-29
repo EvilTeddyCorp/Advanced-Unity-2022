30667f0e2b19421fc4e13e4e7f9e2b7560d0aa22
Markka200 <81672997+Markka200@users.noreply.github.com>
2022-08-29 11:35:38 +0300

    Manager Setup
    + movement

---
+++ Advanced Unity/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    //public GameObject Player;
+
+    bool isPaused = false;
+    public void PauseGame()
+    {
+        if (isPaused)
+            PauseGame(false);
+        else
+            PauseGame(true);
+
+    }
+    void PauseGame(bool t)
+    {
+        if (t)
+        {
+            Time.timeScale = 0;
+            UIManager.Instance.ToggleMenu(true);
+            isPaused = true;
+            //Player.canMove = false;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            UIManager.Instance.ToggleMenu(false);
+            isPaused = false;
+            //Player.canMove = true;
+        }
+
+    }
+}
